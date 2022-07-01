a357083b898f3dc19ef38048063c6b00e02a35c4
postall74 <postall_mcs@mail.ru>
2022-07-02 03:58:42 +0500

    Add  Mathf.MoveTowards

--- Scripts/UI/Health/HelthBarSlider/HealthSlider.cs
+++ Scripts/UI/Health/HelthBarSlider/HealthSlider.cs
@@ -8,18 +8,25 @@ using UnityEngine.UI;
 public class HealthSlider : MonoBehaviour
 {
     [SerializeField] private float _value;
+    [SerializeField] private float _changedSpeed;
 
     private Slider _slider;
+    private float _changedValue;
 
     private void Start()
     {
         _slider = GetComponent<Slider>();
+        _slider.value = _value;
+        _changedValue = _value;
+    }
+    private void Update()
+    {
+        _slider.value = Mathf.MoveTowards(_value, _changedValue, _changedSpeed);
+        _value = _changedValue;
     }
 
     public void OnValueChanged(float value)
     {
-        _value += value;
-        _slider.value = _value;
+        _changedValue += value;
     }
-
 }
