0225d38644b48f1ebee3e6b9dbcf8f1b3ce8daed
postall74 <postall_mcs@mail.ru>
2022-07-04 12:03:09 +0500

    Update Player.cs

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -7,13 +7,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int _maxHealth = 100;
     [SerializeField] private int _minHealth = 0;
-    [SerializeField] private int _changeHealth = 10;
 
     private int _health;
 
     public int MaxHealth => _maxHealth;
     public int MinHealth => _minHealth;
-    public int ChangeHealth => _changeHealth;
     public int CurrentHealth => _health;
 
     public event UnityAction<int> HealthChanged;
@@ -25,27 +23,18 @@ public class Player : MonoBehaviour
         HealthChanged?.Invoke(_health);
     }
 
-    public void TakeDamage()
-    {
-        _health = Mathf.Clamp(_health - _changeHealth, _minHealth, _maxHealth);
-        HealthChanged?.Invoke(_health);
-
-        if (_health <= 0)
-            Die();
-    }
-
     public void TakeDamage(int damage)
     {
-        _health -= Mathf.Clamp(_health - damage, _minHealth, _maxHealth); ;
+        _health = Mathf.Clamp(_health - damage, _minHealth, _maxHealth); ;
         HealthChanged?.Invoke(_health);
 
         if (_health <= 0)
             Die();
     }
 
-    public void TakeHealth()
+    public void TakeHealth(int health)
     {
-        _health = Mathf.Clamp(_health + _changeHealth, _minHealth, _maxHealth);
+        _health = Mathf.Clamp(_health + health, _minHealth, _maxHealth);
         HealthChanged?.Invoke(_health);
     }
 
